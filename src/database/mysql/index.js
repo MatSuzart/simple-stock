5ff39cd2daf334a7707dab7148dd21861200ad3d
MatheusSuzart <mathsuzart@outlook.com>
2023-03-18 13:11:18 -0300

    database-connection

---
+++ src/database/mysql/index.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+ 
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados: ' + err.stack);
+    return;
+  }
+
+  console.log('Conexão bem-sucedida ao banco de dados.');
+});
+
+module.exports = connection;
